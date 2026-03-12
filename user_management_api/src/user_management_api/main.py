ec52a4f7ec097fefd12b729dcd84cfde3b187790
YuliyaS-work <yuliyasorokinawork@gmail.com>
2026-03-12 13:13:32 +0300

    add all for docker and healthcheck

--- user_management_api/src/user_management_api/main.py
+++ user_management_api/src/user_management_api/main.py
@@ -7,3 +7,7 @@ Initializes the FastAPI app and registers all API routers.
 from fastapi import FastAPI
 
 app = FastAPI()
+
+@app.get("/healthcheck")
+async def healthcheck() -> dict:
+    return {"status": "ok"}
