6e2fed28daa6c21fc52c3b36a56fc8561433edb1
Yuliya <yuliyasorokinawork@gmail.com>
2026-03-16 15:21:51 +0300

    Delete user_management_api/test_asyncpg.py

--- user_management_api/test_asyncpg.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import asyncpg
-
-async def main():
-    conn = await asyncpg.connect("postgresql://ums:umn09032026@localhost:5432/ums")
-    print("Connected OK")
-    await conn.close()
-
-asyncio.run(main())
